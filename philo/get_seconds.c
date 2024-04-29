f4e202625f45fbef8a2d2aebd9851c6280db66a9
quietmid <ninjonglu@gmail.com>
2024-04-29 08:54:38 +0300

    time stamp sort it out

--- philo/get_seconds.c
+++ philo/get_seconds.c
@@ -6,7 +6,7 @@
 /*   By: jlu <jlu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 21:49:59 by jlu               #+#    #+#             */
-/*   Updated: 2024/04/26 21:55:20 by jlu              ###   ########.fr       */
+/*   Updated: 2024/04/29 08:53:53 by jlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,24 @@ long long current_timestamp(void)
 	long long		ms;
 
 	gettimeofday(&time, NULL);
-	ms = time.tv_sec * 1000LL + time.tv_usec / 1000;
+	ms = (time.tv_sec * 1000LL) + (time.tv_usec / 1000);
 	return (ms);
 }
+
+// int main ()
+// {
+// 	long long ms_start = current_timestamp();
+// 	long long ms_nw;
+
+// 	printf("time of the day: %lld\n", ms_start);
+// 	sleep(3);
+// 	ms_nw = current_timestamp();
+// 	printf("time of the day: %lld\n", ms_nw - ms_start);
+// 	sleep(3);
+// 	ms_nw = current_timestamp();
+// 	printf("time of the day: %lld\n", ms_nw - ms_start);
+// 	sleep(5);
+// 	ms_nw = current_timestamp();
+// 	printf("time of the day: %lld\n", ms_nw - ms_start);
+// 	return 0;
+// }
